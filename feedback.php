7f58f2aed6daf6bba57cdc2cf0cafefb97d82a25
sumit684 <singh.sumit684@gmail.com>
2019-02-03 22:43:33 +0530

    Create feedback.php

---
+++ feedback.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <title>HOME | Diretorate of Physical Edu and Sports</title>
+        <?php include('include/link.html');?>
+        <style>
+            .body{
+                overflow-y: hidden;
+            }
+        </style>
+
+ </head>
+    <body>
+
+
+        <?php include 'include/nav.html';?>
+        <?php include 'include/header.html';?>
+        <div align="center" >  
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd7LyAJVbJCB5IapJ3EKXSHIRma9uvqAul_sq1V6Ssd9Alymg/viewform?embedded=true" width="100%" height="1100" frameborder="0" marginheight="10px" marginwidth="10px">Loading...</iframe>
+            </div>  
+<?php include 'include/footer.html';?>
+    </body>
+</html>
