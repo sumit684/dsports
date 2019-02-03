49bc7931833313f2516fd4f9f1411c1ededb82f4
sumit684 <singh.sumit684@gmail.com>
2019-02-04 02:42:34 +0530

    Create infrastructure.php

---
+++ infrastructure.php
@@ -0,0 +1,253 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <title>HOME | Diretorate of Physical Edu and Sports</title>
+        <?php include('include/link.html');?>
+         <link href='https://fonts.googleapis.com/css?family=Alex Brush' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family=Bad Script' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family=Carme' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family=Actor' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family=Cutive' rel='stylesheet'>
+
+<style>
+
+/*
+    @media screen and (min-width:746px){
+      .carousel-inner img {
+      width: 100%;
+      height: 500px;
+  }  
+    }
+
+@media screen and(max-width:746x)
+{
+    .carousel-inner img {
+      width: 100%;
+      height: 200px;
+                        }
+
+    }
+*/
+
+
+  </style>
+    </head>
+    <body>
+
+
+        <?php include 'include/nav.html';?>
+        <?php include 'include/header.html';?>
+
+
+
+
+
+        <div class="container-fluid" style="background-color:#e9ecef">  
+
+            <h1  style="color:#1c445e;text-align: center;font-family: 'Cutive';position: relative;top: 10px; ">Infrastructure and Facilities</h1><br>
+            <div id="demo" class="carousel slide" data-ride="carousel">
+
+                <!-- Indicators -->
+                <ul class="carousel-indicators">
+                    <li data-target="#demo" data-slide-to="0" class="active"></li>
+                    <li data-target="#demo" data-slide-to="1"></li>
+                    <li data-target="#demo" data-slide-to="2"></li>
+                    <li data-target="#demo" data-slide-to="3"></li>
+                    <li data-target="#demo" data-slide-to="4"></li>
+                    <li data-target="#demo" data-slide-to="5"></li>
+                    <li data-target="#demo" data-slide-to="6"></li>
+                </ul>
+
+                <!-- The slideshow -->
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="images/infrastructure/sportground2.jpg" alt="Los Angeles">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/infrastructure/7.jpg" alt="Chicago" >
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/infrastructure/41.jpg" alt="New York" >
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/infrastructure/sportground3.jpg" alt="New York" >
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/infrastructure/31.jpg" alt="New York" >
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/infrastructure/43.jpg" alt="Race">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/infrastructure/32.jpg" alt="Baseball" >
+                    </div>
+
+
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                    <span class="carousel-control-prev-icon"></span>
+                </a>
+                <a class="carousel-control-next" href="#demo" data-slide="next">
+                    <span class="carousel-control-next-icon"></span>
+                </a>
+            </div>
+
+
+        </div>
+
+
+        <div class="container-fluid" style="background-color:  #2980b9">
+
+            <div class="row">
+                <div class="col-md-6 jumbotron">
+
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr style="background-color: #2980b9; color: white;">
+                                <th>S.No</th>
+                                <th>SPORTS </th>
+                                <th>TOTAL No.</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Track(400 mts.)Synthetic International class II certified</td>
+                                <td>01</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Foot ball Field (Grass)</td>
+                                <td>01</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>IHockey Field</td>
+                                <td>01</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>Cricket Ground(Turf)</td>
+                                <td>01</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>Basketball courts(Cement)</td>
+                                <td>02</td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                                <td>Tennis courts Asphalt</td>
+                                <td>02</td>
+                            </tr>
+                            <tr>
+                                <td>7</td>
+                                <td>Volley ball courts(Clay)flood lighted</td>
+                                <td>04</td>
+                            </tr>
+                            <tr>
+                                <td>8</td>
+                                <td>Hand ball courts(Clay)</td>
+                                <td>02</td>
+                            </tr>
+                            <tr>
+                                <td>9</td>
+                                <td>Kho-Kho ground</td>
+                                <td>02</td>
+                            </tr>
+                            <tr>
+                                <td>10</td>
+                                <td>Kabaddi ground</td>
+                                <td>02</td>
+                            </tr>
+                            <tr>
+                                <td>11</td>
+                                <td>Multi Gymnassium hall(size 40*24 mts.)</td>
+                                <td>02</td>
+                            </tr>
+                            <tr>
+                                <td>12</td>
+                                <td>Cricket net practice arena turf</td>
+                                <td>03 pitches</td>
+                            </tr>
+
+                            <tr>
+                                <td>13</td>
+                                <td>Multi practice arena</td>
+                                <td>01</td>
+                            </tr>
+
+
+
+
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="col-md-6 jumbotron">
+
+
+                    <div class="container-fluid" style="background-color:">
+
+
+
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr style="background-color:  #2980b9; color: white;">
+
+                                    <th>Year</th>
+                                    <th>Developments </th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>2015-2016</td>
+                                    <td>01 Clay Kho-Kho ground</td>
+
+                                </tr>
+                                <tr>
+                                    <td>2016-2017</td>
+                                    <td>04 Volleyball Flood lighted courts with the help of UGC Grants   </td>
+
+                                </tr>
+                                <tr>
+                                    <td>2016-2017</td>
+                                    <td>Cricket practice turf pitches </td>
+
+                                </tr>
+                                <tr>
+                                    <td>2017-2018</td>
+                                    <td>01 Synthetic Athletic Track International class II certified</td>
+
+                                </tr>
+
+
+
+                            </tbody></table>
+
+                    </div>
+
+
+                    <div class="container mt-3">
+                        <h2 style="background-color: #2980b9; font-family:'Asul';color: white">Future plans of the department</h2>
+
+                        <div class="media border p-3">
+                            <img src="images/infrastructure/42.jpg" alt="Office" class="mr-3 mt-3 rounded-circle" style="width:170px;">
+                            <div class="media-body">
+                                <h4>Construction of the following:-</h4>
+                                <p>a)Swimming pool.<br>b)  60X40 Mts. Size Proposed Multi Purpose hall with facilities for Badminton, Basketball, Volleyball, Table Tennis, Handball Wrestling hall, Shooting range etc.</p>      
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <?php include 'include/footer.html';?>
+    </body>
+</html>
