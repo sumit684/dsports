5a5d63a15a08c44bd10b314624b1f2fb9d971a71
Pratik Mehta <43946536+Pratikmehta1729@users.noreply.github.com>
2019-02-04 15:47:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <style>
 
             .dd{
-                background-image: url(images/visi.jpg);
+                background-image: url(images/blue2.jpg);
                 background-size: cover;
 
                 background-repeat: no-repeat;
