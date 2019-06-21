ca71599b015e18b776c162b50c252631791a2fca
sumit684 <singh.sumit684@gmail.com>
2019-06-21 13:51:43 +0530

    Change in director's mesaage
    updated design by niti in director's message.
    Previous design is named as directormsg2

--- directormsg.php
+++ directormsg.php
@@ -1,125 +1,89 @@
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
 
         <title>Director's Message | Diretorate of Physical Edu and Sports</title>
         <?php include('include/link.html');?>
-        <style type="text/css">
-
-            .ab{
-                background-color: #0f2b57;
 
 
-            }
+        
+  <link href='https://fonts.googleapis.com/css?family=Bitter' rel='stylesheet'>
+ 
+
+     </head>
+ <style type="text/css">
+	
+	.dm2{
+		background-color: white;
+	}
+
+	.dm1{
+	background-image: url("images/blue2.jpg");
+	background-repeat: no-repeat;
+	background-size: cover;
+	}
+
+	.dm3{
+		font-family: "Playfair Display", Arial, serif;
+		font-weight: 400;
+		padding-top: 27px;
+		margin-bottom: -19px;
+		color:#2f2f70;
+		font-size: 50px;
+		text-shadow: 1px 2px 3px black;
+	}
+	.dm4{
+		margin-top: -23px;
+		height: 350px;
+		width: 300px;	
+		border-radius: 50%;
+		box-shadow: 10px 10px 31px -1px #767779;
+		/*border:solid black 2px;*/
+	}
+    .dm5{
+    	text-align: justify;
+    	padding-left: 25px;
+    	padding-right: 25px;
+    	font-size: 18px; 
+    	line-height: 1.6;
+    	font-family:'Bitter';
+    	word-spacing: 5.5px;
+    	padding-bottom: 22px;    
+    }
+
+    	.dm6{
+    		margin-top: 10px;
+    		font-size: 22px;
+    		font-family: serif;;
+    	}
 
 
-            .ru
-            {
-                float:right;
-                width: 900px;
-                height: 600px;
-                background-color:#4267b2;
-            }
-            .ab h1
-            {
-                font-family: 'Carme';
-                color:#d4cdcd;
-            }
-
-            .jumbotron p
-            {color:#031f58;
-                font-size: 15px;
-                font-weight: 100px;
-                padding-right: 200px;
-                padding-left: 200px;
-
-
-            }
-            .jumbotron h1
-            {
-                padding-left: 200px;
-                color: black;
-            }
-            .ab h3
-            {
-                color:#d4cdcd;
-                font-family: 'Actor';
-            }
-            }
-            .row
-            {
-                height: 500px;
-            }
-
-            .jumbotron h1  
-            @media only screen and (max-width: 900px) {
-                .jumbotron p {
-                    padding-right: 50px;
-                    padding-left: 20px;
-                }
-                {
-                    padding-left:20px;
-                    padding-right: 100px;
-
-                }}
-
-        </style>
-    </head>
-    <body>
-
-
-        <?php include 'include/nav.html';?>
+</style>
+<body>
+	<?php include 'include/nav.html';?>
         <?php include 'include/header.html';?>
-        
-        
-        <div class="container-fluid ab">
-            <div class="row ">
-                <div class="col-md-6" style="background-image: 'Sunil.jpg'">
-                    <img src="images/officestaff/1.JPG" style="height: 47
-                                                               0px;width: 60%;margin:5px 0px 0px 5px;margin-left: 11%;"  alt="Deputy Director"  >
-                </div>
-                <div class="col-md-6 text-center" style="background-color: #0f2b57 ;padding-top: 40px;">
-                    <h1>
-                        In India "Game has happened of Game but not Game."</h1> <br><br><br>
-                    <p style=" font-family: 'Alex Brush';font-size: 30px;
-                              border-bottom: solid;
-                              border-color: grey; 
-                              color:#29c231;">
-                        Deep and continuous reflection on this prevailing situation prompted me to initiate this National movement "Play India Play".</p><br>
-                    <h3>Dr. Sunil Dudhale(Deputy Director)</h3>    
-                </div>
-            </div>
-            <div class="row">
+<div class="container-fluid text-center dm1">
+	<h2 class="dm3">Director's message</h2>
+	<div style="height: 20px;"></div>
+	<div class="container">
+<div class="jumbotron dm2">
+<img src="images/officestaff/1.JPG" alt="Avatar" class="img-responsive dm4">
+	<div class="dm6 font-weight-bold"> DR. SUNIL DUDHALE </div>
+	<h6>(Deputy Director)</h6>
+<hr size="30" style="width:50%;">
+<div class="dm5">Sports unify people together irrespective of there age, gender and religion. They also bring out the best among individual and give us human being of extra capabilities,  ones that leaves their marking the history book for ever. While lot of Countries have had a successful Sporting Culture, India has always being a Sports Loving Country but not a Sports Playing Country.
 
-                <div class="col-md-12 jumbotron">
-                    <h1>Playing India and Great India</h1><br>
+	Over the last few years India has seen Sports boom like never before. Young achievers in a variety of Sports has grown with an increasing number of Medals being won at Olympics, Asian Games, Common Wealth Games and other International events. 
 
-                    <p>It is extremely unfortunate that in spite of cultural and religious importance of play we have miserably failed at present to give appropriate place to "Play" in our social and educational system. Play (physical activity) was part and parcel of our society since time immemorial. Students were taught games & sports and physical activities during their education in Gurukuls. The importance of play is revealed from the very fact that the marriage (Swamvar) conditions of Sitaji & Draupadiji were putting the string on the Pinak bow & piercing the eye of the rotating fish respectively. There were many options available like discussion on shastra, creation of a best poem, solving a problem of math's etc., but selection of Physical prowess & skill as condition reflects very clearly the importance it had then.</p>
-                   
-                    <p>  I wonder how & why we are not considering Physical activity (Play) important. If the situation of increasing non activity is not tackled honestly then it will be just impossible to eradicate the ill effects. The correction should be made as soon as possible so that damage process is checked/stopped as soon as possible before it is too late.</p>
-                   
-                    <p>  Physical activity is the need of the body & it should not be denied. If India will not play today than it is going to be a weak nation tomorrow.</p>
-                   
-                    <p>"Play India Play" is a "National Moment", a "National Cause" and needs support & patronage of each and every citizen of India so that as soon as possible "Play" becomes the right of each & every citizen of India. We always talk about the India's unity in diversity & "Play" is going to be a strong binding catalyst to unite India.
+	At Devi Ahilya Vishwavidyalaya We offers a variety of Games and Sports for Students. Our ultimate goal is to increase the participation in Sports. I believe Sports provide the required exposure for the students to grow and mature to adulthood. Sports helps to inculcate Team Work, Commitment, Discipline Leadership, Hard Work, Tufness, Sportsmanship, Sacrifice and above  all Character building. Sports prepare individuals for the Journey of Life that lies ahead of them. 
 
-                        The imagination of success of this movement brings in my mind lakhs of job opportunities, mentally-physically & socially fit citizens, play facilities at every nook & corner and a very strong "United Nation through Games & Sports."
+	I request every one our Students, Staff, Parents and Faculties to dedicate at least 40 minutes per day for your self and utilized this time on the Playfield, Gymnasium or in Park for their healthy and fit life. 
+</div>
+	
 
-                    </p>
-                    <br>
-                    <p>  Please, come forward open heartedly without any term or condition to SUPPORT this noble movement in your best way.
-
-                        Without "You" it is not going to be successful movement.</p>
-                    <br>
-
-                    <p style="color: black " >Dr. Sunil Dudhale</p >
-
-                </div>
-            </div>
-
-
-        </div>
-        
-        <?php include 'include/footer.html';?>
-    </body>
-</html>
+</div>
+</div>
+</div> 
+ <?php include 'include/footer.html';?>
+</body>
+</html>  
