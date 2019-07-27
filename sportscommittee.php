4a488f033d3699716d2dd63b852749a349b33a94
Nitss10 <46421818+Nitss10@users.noreply.github.com>
2019-07-27 13:04:22 +0530

    my changes

---
+++ sportscommittee.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sports Committee | Directorate of Physical Education & Sports</title>
+	<?php include 'include/link.html'?>
+	<link rel="stylesheet" type="text/css" media="screen" href="stylesheets/panel.css">
+    <style type="text/css">
+        fieldset.scheduler-border {
+    border: 1px groove #ddd !important;
+    padding: 0 1.4em 1.4em 1.4em !important;
+    margin: 0 0 1.5em 0 !important;
+    -webkit-box-shadow:  0px 0px 0px 0px #000;
+            box-shadow:  0px 0px 0px 0px #000;
+}
+
+legend.scheduler-border {
+    width:inherit; /* Or auto */
+    padding:15px 20px; /* To give a bit of padding on the left and right */
+    border-bottom:none;
+}
+    </style>
+</head>
+
+<body>
+	<?php include 'include/nav.html'?>
+	<?php include 'include/header.html'?>
+    <div class="container" >
+        <fieldset class="scheduler-border">
+    <legend class="scheduler-border" align="center" style="font-size: 50px; color: #17557e; text-transform: uppercase;">Sports Committee</legend>
+     
+    <table class="calendar table" id="myTable">
+                        <thead>
+                        <tr class="table-primary"><th valign="top" >SNO.</th>
+                            <th valign="top">NAME</th>
+                            <th valign="top">DESIGNATION</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                        <td>1</td>
+                            <td>Manniya Kulpati ji </td>
+                            <td>Chairman</td>
+                            
+                        </tr>
+                        <tr>
+                        <td>2</td>
+                            <td>Dr. (Mrs) S. Waskel</td>
+                            <td>Member</td>
+                            
+                        </tr>
+                        <tr>
+                        <td>3</td>
+                            <td>Dr. Anil Kapdia</td>
+                            <td>Member</td>
+                            
+                        </tr>
+                        <tr>
+                        <td>4</td>
+                            <td>Dr. Ashok Kumar Sharma</td>
+                            <td>Member</td>
+                            
+                        </tr>
+                        <tr>
+                        <td>5</td>
+                            <td>Dr. Sanjay Tanwani</td>
+                            <td>Member</td>
+                            
+                        </tr>
+                        <tr>
+                        <td>6</td>
+                            <td>Dr. Deepak Mehta</td>
+                            <td>Member</td>
+                            
+                        </tr>
+                        <tr>
+                        <td>7</td>
+                            <td>Dr. S.N.Yadav</td>
+                            <td>Member</td>
+                            
+                        </tr>
+                        <tr>
+                        <td>8</td>
+                            <td>Dr. Harbans Singh </td>
+                            <td>Member</td>
+                            
+                        </tr>
+                        <tr>
+                        <td>9</td>
+                            <td>Dr. S.L.Dewda</td>
+                            <td>Member</td>
+                            
+                        </tr>
+                        <tr>
+                        <td>10</td>
+                            <td>Dr. Usha Jain</td>
+                            <td>Member</td>
+                            
+                        </tr>
+                        <tr>
+                        <td>11</td>
+                            <td>Prof. Rafi Mohammed Shaikh</td>
+                            <td>Member</td>
+                            
+                        </tr>
+                        <tr>
+                        <td>12</td>
+                            <td>Dr. Preeti Singh</td>
+                            <td>Member</td>
+                            
+                        </tr>
+                        <tr>
+                        <td>13</td>
+                            <td>Dr. Mahendra Kumar Mishra</td>
+                            <td>Member</td>
+                            
+                        </tr>
+                        <tr>
+                        <td>14</td>
+                            <td>Dr. Nilesh Mandloi</td>
+                            <td>Member</td>
+                            
+                        </tr>
+                        <tr>
+                        <td>15</td>
+                            <td>Dr. Avinash Yadav</td>
+                            <td>Member</td>
+                            
+                        </tr>
+                        <tr>
+                        <td>16</td>
+                            <td>Shri Sandeep Shrivas</td>
+                            <td>Member</td>
+                            
+                        </tr>
+                        <tr>
+                        <td>17</td>
+                            <td>Dr. Sunil Dudhale
+(Incharge Director Physical Education.)</td>
+                            <td>Member-Secretary </td>
+                            
+                        </tr></tbody></table>
+   
+</fieldset>
+           
+      <div align="center">   
+    <a href="downloads/committee/SportsCommittee2018-2021.pdf" target="_blank" style="color: white "><button class="btn btn-primary" > <i class="fa fa-download"> </i>&nbsp;Download PDF</button></a>
+    </div>  
+    </div>
+	
+	<?php include 'include/footer.html';?>
+
+</body>
+</html>
