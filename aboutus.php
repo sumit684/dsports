b64ce0b42bd85737661e3fbba07633d6c8accb30
sumit684 <singh.sumit684@gmail.com>
2019-07-30 23:43:38 +0530

    changes

--- aboutus.php
+++ aboutus.php
@@ -29,22 +29,32 @@
 	<div class="mycontent container">
 		<h1 style="font-family: 'Asul', cursive;"><center>About Directorate</center></h1>
 		<hr>
-		<p>
-			<span class="tab"></span><b>Indore</b> is known as the capital of Sports and Games in Madhya Pradesh State. Devi Ahilya Vishwavidyalaya (previously known as Indore University) has also its own history and tradition in Games & Sports in the State. Our university teams achieved many position in Kho-Kho, Kabaddi, Wrestling, Cricket, Badminton, Table Tennis, Tennis, Softball, Diving, Gymnastics & many other games at State, Zonal and All India level.
-		</p>
-		<p>
-			<span class="tab"></span>The Directorate of Physical Education & Sports is one of the core and important departments of Devi Ahilya Vishwavidyalaya. The core mission of the department is to provide a broad spectrum of the Sports, Recreation, Wellness and Leisure activities to our students as well as faculties and staff also.
-		</p>
-		<p>
-			<span class="tab"></span>Currently we are catering 33 games in men section and 30 games in women section at Inter Collegiate as well as Inter Varsity Zonal / All India level. Department has quite good infrastructure, International Synthetic Athletics Track, Turf Cricket ground, Grassy Football ground, Flood lighted Volleyball Courts, Cemented Basketball Courts, Archery field, Tennis court, Hockey field, Kho-Kho and Kabaddi fields. Apart from these fields a Multipurpose Gymnasium Hall is also there in which Badminton & Gymnastics facilities are provided.             
-		</p>
-		<p>
-			From the last 2-3 years we are conducting regular coaching cum combined practice activities in Basketball, Football, Volleyball and rack & Filed. Very soon we will launch Indian round Archery activity also.     
-		</p>
-		<p>
-			Our department endeavor to  contribute towards the compliment of the University Mission,Vision and Objectives for this we set the Platform and the Standards of Sports and Games services rendered to the students with a view to attain a holistic student who is a team player and leader in the society at large.
-		</p>
-		<i>Thanks</i>
+		<div class="row">
+			<div class="col-md-12"align="center" >
+				<img src="images/office2.jpg" alt="Directorate of Physical Education & Sports" class="img-responsive img-fluid" style="height: 300px;">
+			</div>
+		</div>
+		<hr>
+		<div class="row">
+			<div class="col-md-12">
+				<p>
+					<span class="tab"></span><b>Indore</b> is known as the capital of Sports and Games in Madhya Pradesh State. Devi Ahilya Vishwavidyalaya (previously known as Indore University) has also its own history and tradition in Games & Sports in the State. Our university teams achieved many position in Kho-Kho, Kabaddi, Wrestling, Cricket, Badminton, Table Tennis, Tennis, Softball, Diving, Gymnastics & many other games at State, Zonal and All India level.
+				</p>
+				<p>
+					<span class="tab"></span>The Directorate of Physical Education & Sports is one of the core and important departments of Devi Ahilya Vishwavidyalaya. The core mission of the department is to provide a broad spectrum of the Sports, Recreation, Wellness and Leisure activities to our students as well as faculties and staff also.
+				</p>
+				<p>
+					<span class="tab"></span>Currently we are catering 33 games in men section and 30 games in women section at Inter Collegiate as well as Inter Varsity Zonal / All India level. Department has quite good infrastructure, International Synthetic Athletics Track, Turf Cricket ground, Grassy Football ground, Flood lighted Volleyball Courts, Cemented Basketball Courts, Archery field, Tennis court, Hockey field, Kho-Kho and Kabaddi fields. Apart from these fields a Multipurpose Gymnasium Hall is also there in which Badminton & Gymnastics facilities are provided.             
+				</p>
+				<p>
+					From the last 2-3 years we are conducting regular coaching cum combined practice activities in Basketball, Football, Volleyball and rack & Filed. Very soon we will launch Indian round Archery activity also.     
+				</p>
+				<p>
+					Our department endeavor to  contribute towards the compliment of the University Mission,Vision and Objectives for this we set the Platform and the Standards of Sports and Games services rendered to the students with a view to attain a holistic student who is a team player and leader in the society at large.
+				</p>
+				<i>Thanks</i>
+			</div>
+		</div>
 	</div>
 	<?php include 'include/footer.html';?>
 </body>
--- index.php
+++ index.php
@@ -197,13 +197,15 @@
                 </div>
             </div>
             <div class="col-md-7" style="color:#3b2f2f;  padding: 20px; padding-left: 50px;">
-                <h1 style="font-family: 'Asul', cursive;"><center>About DAVV</center></h1>
+                <h1 style="font-family: 'Asul', cursive;"><center>About Directorate</center></h1>
                 <hr>
-                <div style="font-family: 'Brawler',sans-serif; text-align:justify;font-size: 20px;">Devi Ahilya Vishwavidyalaya (DAVV),
+                <div style="font-family: 'Brawler',sans-serif; text-align:justify;font-size: 20px;">
+                    Devi Ahilya Vishwavidyalaya (DAVV),
                     formerly University of Indore, is a premier University in Central India, has completed 54th years 
                     of establishment, educational commitment and services. It is the First State University of Madhya
                     Pradesh which has been accredited with “A” grade by NAAC. It was established in 1964, by an
-                Act of Legislature of Madhya Pradesh. </div>        
+                Act of Legislature of Madhya Pradesh. 
+            </div>        
             </div>
         </div>
 
